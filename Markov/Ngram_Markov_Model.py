89f013a26d22b50621ab71668de611849499ed42
Elle O'Brien <andronovhopf@gmail.com>
2019-10-21 16:57:18 -0700

    Update Ngram_Markov_Model.py

--- Markov/Ngram_Markov_Model.py
+++ Markov/Ngram_Markov_Model.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  4 21:21:00 2018
-
-@author: andro
-"""
 
 # -*- coding: utf-8 -*-
 """
@@ -22,7 +16,7 @@ import numpy as np
 import random as random
 
 ###############################################################################
-##                  RECIPE ANALYSIS                                          ##
+##                  MARKOV MODEL TO GENERATE RECIPES                            ##
 ###############################################################################
 filename = "Ing_Recipe_Aggregate.txt"
 
@@ -32,9 +26,7 @@ with open(filename, encoding = 'utf-8') as f:
 corpus = corpus.lower()
 corpus.replace('\n',';')
 
-# Get the first three words of each document
-
-
+# Tokenize each document and get bi-,tri-,four-, and five-grams.
 token = word_tokenize(corpus)
 bigrams = list(ngrams(token,2))
 trigrams = list(ngrams(token,3))
@@ -123,106 +115,3 @@ out_str =out_str.replace('break', '\n\n')
 with open('Markov_Model_Out.txt', 'w') as thefile:
     thefile.write(out_str)
 
-    
-###### Five grams #########
-    ###### Four grams ###############
-start1 = '2.0'
-start2 = 'teaspoon'
-start3 = 'baking'
-start4 = 'soda'
-
-model_text = [start1,start2,start3,start4]
-for i in range(0,500):
-    # Find all elements in the bigram corpus that start
-    # with startword
-    cumsum = [];
-    gram_choices = [];
-    choices = [i for i, j in enumerate(fivegrams) if j[0]==start1 and j[1] == start2 and j[2] == start3
-               and start4 == j[3]]
-    # Get all the bigrams that occur
-    newlist = [fivegrams[i] for i in choices]
-    # Get the most popular one
-    e2 = Counter(newlist)
-    # Get the most common n-gram from this model 
-        # Get the cumulative probabilities of 
-    for k,v in e2.items():
-        cumsum.append(v)
-        gram_choices.append(k)
-    cumsum = np.array([x / sum(cumsum) for x in cumsum])
-    cumsum = np.cumsum(cumsum)
-    # Generate a random number
-    r = random.random()
-    ind = np.min(np.where(cumsum > r))
-    # Get the most common n-gram from this model 
-    new_word = gram_choices[ind][4]
-
-    start1 = start2
-    start2 = start3
-    start3 = start4
-    start4 = new_word
-    model_text.append(new_word)
-
-
-# Print to file!
-with open('test.txt', 'w') as thefile:
-    thefile.write(" ".join(model_text))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def is_number(n):
-    try:
-        float(n)   # Type-casting the string to `float`.
-                   # If string is not a valid `float`, 
-                   # it'll raise `ValueError` exception
-    except ValueError:
-        return False
-    return True    
-################################## Four grams ################################
-ingredient_list = ["butter","flour","eggs","baking",
-                   "chocolate","walnuts","brown","sugar",
-                   "vanilla","salt"]
-
-
-for item in ingredient_list:
-    # Find all elements in the bigram corpus that start
-    # with startword
-    choices = [i for i, j in enumerate(fourgrams) if j[0]==item or j[1] == item or j[2] == item]
-    # Get all the bigrams that occur
-    newlist = [fourgrams[i] for i in choices]
-    # Get the most popular one
-    d2 = Counter(newlist)
-    # Go down the list until a number appears in the n-gram
-    fetch = 1
-    has_num = 0
-    while has_num == 0:
-        check = d2.most_common(fetch)
-        if is_number(check[fetch-1][0][0]):
-            start_gram = check[fetch-1]
-            has_num = 1
-        fetch = fetch+1
-    print(start_gram)
-
-
-###############################################################################
-##               INGREDIENT ANALYSIS                                         ##
-###############################################################################
